d7445587caff9b1ad53b6244c21af9481ae134bd
GregBlockx <greg.blockx@hotmail.com>
2017-10-08 16:20:50 +0200

    Update layout start screen and navigation + layout work screen

---
+++ BecomeRich/app/src/main/java/com/blockx/greg/becomerich/WorkActivity.java
@@ -0,0 +1,13 @@
+package com.blockx.greg.becomerich;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WorkActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_work);
+    }
+}
