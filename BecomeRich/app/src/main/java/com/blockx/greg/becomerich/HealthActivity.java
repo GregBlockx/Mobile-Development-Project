864ad51d8cde9d044f420e1af1d1e9e65310cd4d
GregBlockx <greg.blockx@hotmail.com>
2017-10-16 13:54:17 +0200

    Substract money when buy food and health

--- BecomeRich/app/src/main/java/com/blockx/greg/becomerich/HealthActivity.java
+++ BecomeRich/app/src/main/java/com/blockx/greg/becomerich/HealthActivity.java
@@ -4,7 +4,13 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.CheckBox;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
@@ -14,6 +20,9 @@ public class HealthActivity extends AppCompatActivity {
     ListView listview;
     ActivityAdapter activityAdapter;
     ArrayList<Activity> healthList = new ArrayList<>();
+    int yourMoneyInt;
+    int healthPrice;
+    TextView yourMoney;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +30,10 @@ public class HealthActivity extends AppCompatActivity {
         setContentView(R.layout.activity_health);
 
         listview = (ListView) findViewById(R.id.listViewHealth);
+        yourMoney = (TextView) findViewById(R.id.textViewYourMoney);
+
+        //Test to buy something
+        yourMoney.setText("€ 1000");
 
         healthList.add(new Activity("Sleep on road", 0));
         healthList.add(new Activity("Take a Pill", 2));
@@ -32,6 +45,24 @@ public class HealthActivity extends AppCompatActivity {
 
         activityAdapter = new ActivityAdapter(this, R.layout.activityrow, healthList);
         listview.setAdapter(activityAdapter);
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                yourMoneyInt = Integer.parseInt(yourMoney.getText().toString().substring(2));
+                healthPrice = Integer.parseInt(adapterView.getItemAtPosition(i).toString());
+
+                if(yourMoneyInt >= healthPrice){
+                    yourMoneyInt -= healthPrice;
+                }else if(yourMoneyInt < healthPrice){
+                    Toast toast = Toast.makeText(getApplicationContext(), "You don't have enough money!", Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+
+                yourMoney.setText("€ " + yourMoneyInt);
+
+            }
+        });
     }
 
     public void goBackToScreen(View view){
--- BecomeRich/app/src/main/java/com/blockx/greg/becomerich/HungerActivity.java
+++ BecomeRich/app/src/main/java/com/blockx/greg/becomerich/HungerActivity.java
@@ -4,7 +4,11 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import java.util.ArrayList;
 
 public class HungerActivity extends AppCompatActivity {
@@ -12,6 +16,9 @@ public class HungerActivity extends AppCompatActivity {
     ListView listview;
     ActivityAdapter activityAdapter;
     ArrayList<Activity> foodList = new ArrayList<>();
+    int yourMoneyInt;
+    int foodPrice;
+    TextView yourMoney;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +26,10 @@ public class HungerActivity extends AppCompatActivity {
         setContentView(R.layout.activity_eat);
 
         listview = (ListView) findViewById(R.id.listViewFood);
+        yourMoney = (TextView) findViewById(R.id.textViewYourMoney);
+
+        //Test to buy something
+        yourMoney.setText("€ 1000");
 
         foodList.add(new Activity("Eat Trash", 0));
         foodList.add(new Activity("Eat Nuts", 2));
@@ -30,6 +41,24 @@ public class HungerActivity extends AppCompatActivity {
 
         activityAdapter = new ActivityAdapter(this, R.layout.activityrow, foodList);
         listview.setAdapter(activityAdapter);
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                yourMoneyInt = Integer.parseInt(yourMoney.getText().toString().substring(2));
+                foodPrice = Integer.parseInt(adapterView.getItemAtPosition(i).toString());
+
+                if(yourMoneyInt >= foodPrice){
+                    yourMoneyInt -= foodPrice;
+                }else if(yourMoneyInt < foodPrice){
+                    Toast toast = Toast.makeText(getApplicationContext(), "You don't have enough money!", Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+
+                yourMoney.setText("€ " + yourMoneyInt);
+
+            }
+        });
     }
 
     public void goBackToScreen(View view){
