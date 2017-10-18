1deab1cbfc800fd77c4153cc43f68fe7bc91d246
BryanDeferm <bryan.deferm@gmail.com>
2017-10-18 13:12:03 +0200

    Added Facebook API Login

--- BecomeRich/app/src/main/java/com/blockx/greg/becomerich/MainActivity.java
+++ BecomeRich/app/src/main/java/com/blockx/greg/becomerich/MainActivity.java
@@ -5,20 +5,74 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.view.ActionMode;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 public class MainActivity extends AppCompatActivity {
     private SharedPreferences sharedPreferences;
     private SharedPreferences.Editor editor;
     private static int maxHealthAndHunger = 300;
+    LoginButton loginButton;
+    CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions("email","public_profile");
+
+        callbackManager = CallbackManager.Factory.create();
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                String userid = loginResult.getAccessToken().getUserId();
+
+                GraphRequest graphRequest = GraphRequest.newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(JSONObject object, GraphResponse response) {
+                        try {
+                            displayUserInfo(object);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "first_name, last_name, email,id");
+                graphRequest.setParameters(parameters);
+                graphRequest.executeAsync();
+
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
 
         Context context = getApplicationContext();
 
@@ -77,4 +131,22 @@ public class MainActivity extends AppCompatActivity {
         Intent startHealthActivity = new Intent(this, HealthActivity.class);
         startActivity(startHealthActivity);
     }
+
+    public void displayUserInfo(JSONObject object) throws JSONException {
+        String firstName, lastName, Email, ID;
+        firstName = object.getString("first_name");
+        lastName = object.getString("last_name");
+        Email = object.getString("email");
+        ID = object.getString("id");
+
+        TextView lastNameTextView = (TextView) findViewById(R.id.textViewName);
+        lastNameTextView.setText("Mr. " + lastName);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
 }
